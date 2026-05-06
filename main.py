a0babce455b79d2702633ccba0145124598d0823
Natalie Huynh <n3huynh@ucsd.edu>
2026-05-06 16:05:11 -0700

    break things

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+#unused import
+
+import os
 def main():
+    unused_var = 123
     print("Hello from github-actions!")
 
+def bad_params( x,y ):
+    print( "bad formatting and params" );
 
 if __name__ == "__main__":
     main()
