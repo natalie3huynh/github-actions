7850e0abc612e798753cf3815d7fff773e8200a0
Natalie Huynh <n3huynh@ucsd.edu>
2026-05-06 16:19:21 -0700

    Add pre-commit configuration

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-#unused import
+# unused import
+
 
 def main():
     print("Hello from github-actions!")
 
+
 def bad_params():
-    print("empty function")
+    x = 1 + 2
+    print(x)
+
 
 if __name__ == "__main__":
     main()
